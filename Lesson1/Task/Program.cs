dc4d413fb72db78476c7792c662cc070fb779bdb
Mila00 <kallion.luidmila@gmail.com>
2024-10-11 22:36:08 +0300

    added updates

--- Lesson1/Task/Program.cs
+++ Lesson1/Task/Program.cs
@@ -11,7 +11,26 @@
 
 // То же самое, но проще, без дополнительной переменной
 
-System.Console.Write("Введите первое число: ");
-int firstnumber  =  int.Parse(Console.ReadLine());
+// System.Console.Write("Введите первое число: ");
+// int firstnumber  =  int.Parse(Console.ReadLine());
 
-System.Console.WriteLine(firstnumber +10);
+// System.Console.WriteLine(firstnumber +10);
+
+
+
+//Пример объединения двух задач- попросить ввести данные и вывести ответ
+System.Console.Write("Введите первое число: ");
+// int firstnumber  =  int.Parse(Console.ReadLine()); // Этот конвектор выдает предупреждения, если пользователь введет неправильные данные, то результатом будет пустота. Чтобы этого избежать используем второй конвектор
+int firstnumber  =  Convert.ToInt32(Console.ReadLine()); // все то же самое, но не будут выводить предупреждения в терминал
+
+System.Console.Write("Введите второе число: ");
+int secondnumber = int.Parse(Console.ReadLine());
+
+if(firstnumber == secondnumber * secondnumber) // true
+{
+System.Console.WriteLine("a = " + firstnumber + " , b = " + secondnumber + " => да");
+}
+else
+{
+System.Console.WriteLine($"a = {firstnumber}, b = {secondnumber} => нет");
+}
