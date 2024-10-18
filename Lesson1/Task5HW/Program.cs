621087d798f3fc61c5ac2246fbb20ed8a7dc9cf7
Mila00 <kallion.luidmila@gmail.com>
2024-10-18 15:38:34 +0300

    added Task5HW

---
+++ Lesson1/Task5HW/Program.cs
@@ -0,0 +1,60 @@
+﻿// Напишите метод, который на вход принимает два целых числа и проверяет,
+// делится ли первое число на второе. Если делится, выводите "делится", иначе
+// выводите "не делится".
+
+
+// System.Console.Write("Введите первое целое число: ");
+// int firstnumber = Convert.ToInt32(Console.ReadLine());
+
+// System.Console.Write("Введите второе целое число: ");
+// int secondnumber = Convert.ToInt32(Console.ReadLine());
+
+
+// if(firstnumber % secondnumber == 0)
+// {
+//     System.Console.WriteLine("Делится");
+// }
+// else
+// {
+//     System.Console.WriteLine("Не делиться");
+// }
+
+// Напишите метод, который принимает на вход три числа и возвращает
+// среднее из этих чисел (то есть не самое большое и не самое маленькое).
+
+// System.Console.Write("Введите первое число: ");
+// int firstnumber = Convert.ToInt32(Console.ReadLine());
+
+// System.Console.Write("Введите второе число: ");
+// int secondnumber = Convert.ToInt32(Console.ReadLine());
+
+// System.Console.Write("Введите третье число: ");
+// int thirdnumber = Convert.ToInt32(Console.ReadLine());
+
+// int sum = firstnumber + secondnumber + thirdnumber;
+// int result = sum / 3;
+// System.Console.WriteLine(result);
+
+// Исправить код
+
+// System.Console.Write("Введите первое число: ");
+// int firstnumber = Convert.ToInt32(Console.ReadLine());
+
+// System.Console.Write("Введите второе число: ");
+// int secondnumber = Convert.ToInt32(Console.ReadLine());
+
+// System.Console.Write("Введите третье число: ");
+// int thirdnumber = Convert.ToInt32(Console.ReadLine());
+
+// if((firstnumber >= secondnumber && firstnumber <= thirdnumber) || (firstnumber >= thirdnumber && firstnumber <= secondnumber))
+// {
+//     System.Console.Write(firstnumber);
+// }
+// else if((secondnumber >= firstnumber && secondnumber <= thirdnumber)) || (secondnumber >= thirdnumber && secondnumber <= firstnumber))
+// {
+//     System.Console.Write(secondnumber);
+// }
+// else
+// {
+//     System.Console.Write(thirdnumber);
+// }
